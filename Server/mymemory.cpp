97286edf64b318f6e1b66846d0b505795707c4db
Quentin <qkabasele@gmail.com>
2016-05-11 13:10:31 +0200

    Le serveur répond en envoyant les données CPU et Mémoire du client à celui-ci

--- Server/mymemory.cpp
+++ Server/mymemory.cpp
@@ -24,34 +24,37 @@ double MyMemory::getMemory()
 
         if((fp = fopen(NomFic, "r")) == NULL)   cout << "Erreur dans la lecture du fichier!" << endl;
 
-        cout << "Voici l'utilisation de la mémoire: " << endl;
-        //MemTotal
-        fscanf(fp, "%s %ld %s", nom, &v1, c);
-        printf("%s %ld %s\n", nom, v1, c);
 
-        //MemFree
-        for(int i=0; i<1; ++i){
-            fscanf(fp, "%s %ld %s", nom, &v2, c);
-            printf("%s %ld %s\n", nom, v2, c);
-        }
+            cout << "Voici l'utilisation de la mémoire: " << endl;
+            //MemTotal
+            fscanf(fp, "%s %ld %s", nom, &v1, c);
+            printf("%s %ld %s\n", nom, v1, c);
 
-        //Buffer
-        for(int i=3; i<=4; ++i){
-            fscanf(fp, "%s %ld %s", nom, &v3, c);
-        }
-        printf("%s %ld %s\n", nom, v3, c);
+            //MemFree
+            for(int i=0; i<1; ++i){
+                fscanf(fp, "%s %ld %s", nom, &v2, c);
+                printf("%s %ld %s\n", nom, v2, c);
+            }
 
-        //Cache
-        for(int i=3; i<4; ++i){
-            fscanf(fp, "%s %ld %s", nom, &v4, c);
-        }
-        printf("%s %ld %s\n", nom, v4, c);
-        /*sleep(5);
-        rewind(fp);*/
+            //Buffer
+            for(int i=3; i<=4; ++i){
+                fscanf(fp, "%s %ld %s", nom, &v3, c);
+            }
+            printf("%s %ld %s\n", nom, v3, c);
 
-        //Mémoire utilisée
-        memRes = v2+v3+v4;
-        res = (double)(v1-memRes);
+            //Cache
+            for(int i=3; i<4; ++i){
+                fscanf(fp, "%s %ld %s", nom, &v4, c);
+            }
+            printf("%s %ld %s\n", nom, v4, c);
+            /*sleep(5);
+            rewind(fp);*/
 
-        return res;
+            //Mémoire utilisée
+            memRes = v2+v3+v4;
+            res = (double)(v1-memRes);
+
+            return res;
+
+
 }
