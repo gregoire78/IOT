8bd9486a19ea7c28b8607189473b212af27b4030
lnvs75 <loic.neves@ynov.com>
2016-05-12 18:21:08 +0200

    Flux Entrant et Sortant

--- Server/flux.h
+++ Server/flux.h
@@ -1,11 +1,19 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+
 #ifndef FLUX_H
 #define FLUX_H
+
+using namespace std;
 
 class flux
 {
 public:
     flux();
-    int fluxet();
+    string fluxentrant();
+    string fluxsortant();
+
 };
 
 #endif // FLUX_H
--- Server/main.cpp
+++ Server/main.cpp
@@ -2,29 +2,41 @@
 #include "myserver.h"
 #include "mycpu.h"
 #include "mymemory.h"
+#include <string>
+#include <string.h>
+#include <iostream>
 
 #include "flux.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-   // MyServer Server;
-    //MyMemory memory;
-    //MyCPU *myCpu;
+    /*MyServer Server;
+    MyMemory memory;
+    MyCPU *myCpu;*/
 
-    double v;
-    //double b;
+    /*double v;
+    double b;*/
 
     /*v = memory.getMemory();
-    printf("La mémoire utilisée est de: %F\n\n", v);*/
+    printf("La mémoire utilisée est de: %F\n\n", v);
 
-    /*b = myCpu->cpuPcent();
-    printf("Le Pcentage CPU instantanne est %g\n", b);*/
-    //myCpu->liste();
+    b = myCpu->cpuPcent();
+    printf("Le Pcentage CPU instantanne est %g\n", b);
+    myCpu->liste();*/
+
     flux myFlux;
-    int f;
-    f = myFlux.fluxet();
+    string e;
+    string s;
+    e = myFlux.fluxentrant();
+    cout<< "flux entrant :"<< e << endl;
+
+    s = myFlux.fluxsortant();
+    cout<< "flux sortant :"<< s << endl;
+
 
     return a.exec();
 }
