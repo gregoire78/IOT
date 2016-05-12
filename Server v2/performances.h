b85e3503d6f221701e85b182584f7f4685827be2
Quentin <qkabasele@gmail.com>
2016-05-12 19:08:28 +0200

    Lancement du serveur par interface graphique: création d'un serveur v2

---
+++ Server v2/performances.h
@@ -0,0 +1,29 @@
+#ifndef PERFORMANCES_H
+#define PERFORMANCES_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class performances;
+}
+
+class performances : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit performances(QWidget *parent = 0);
+    ~performances();
+
+private slots:
+    void on_lcdNumberCpu_overflow();
+
+    void on_doubleSpinBox_valueChanged(double arg1);
+
+    void on_doubleSpinBoxCPU_valueChanged(double arg1);
+
+private:
+    Ui::performances *ui;
+};
+
+#endif // PERFORMANCES_H
